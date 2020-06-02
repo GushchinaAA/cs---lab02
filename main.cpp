588610cf238d2b9b64aad166893bca1d5a26b137
Alice (GushchinaAA) <alice@example.com>
2020-06-03 00:43:05 +0400

    code: изменили тип переменных

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B: ";
-    int a, b;
+    double a, b;
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
     << "A - B = " << a - b << '\n'
@@ -13,6 +13,12 @@ int main()
     << "a * b = " << a * b << '\n'
     << "A / B = " << a / b;
 
+
+    int min;
+    if (a<b)
+    min = a;
+    else
+    min = b;
     int max = a;
     if (a>b)
     max = a;
